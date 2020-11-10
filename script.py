892db4f6120f15826ec949eece0534b34481fdb1
TheSidePorch <jpporch@gmail.com>
2020-11-09 21:40:55 -0500

    formatting

--- script.py
+++ script.py
@@ -20,12 +20,16 @@ months = [
 ]
 
 # years of hurricanes
-years = [1924, 1928, 1932, 1932, 1933, 1933, 1935, 1938, 1953, 1955, 1961, 1961, 1967, 1969, 1971, 1977, 1979, 1980,
-         1988, 1989, 1992, 1998, 2003, 2004, 2005, 2005, 2005, 2005, 2007, 2007, 2016, 2017, 2017, 2018]
+years = [
+    1924, 1928, 1932, 1932, 1933, 1933, 1935, 1938, 1953, 1955, 1961, 1961, 1967, 1969, 1971, 1977, 1979, 1980,
+    1988, 1989, 1992, 1998, 2003, 2004, 2005, 2005, 2005, 2005, 2007, 2007, 2016, 2017, 2017, 2018
+]
 
 # maximum sustained winds (mph) of hurricanes
-max_sustained_winds = [165, 160, 160, 175, 160, 160, 185, 160, 160, 175, 175, 160, 160, 175, 160, 175, 175, 190, 185,
-                       160, 175, 180, 165, 165, 160, 175, 180, 185, 175, 175, 165, 180, 175, 160]
+max_sustained_winds = [
+    165, 160, 160, 175, 160, 160, 185, 160, 160, 175, 175, 160, 160, 175, 160, 175, 175, 190, 185,
+    160, 175, 180, 165, 165, 160, 175, 180, 185, 175, 175, 165, 180, 175, 160
+]
 
 # areas affected by each hurricane
 areas_affected = [['Central America', 'Mexico', 'Cuba', 'Florida', 'The Bahamas'],
@@ -56,14 +60,18 @@ areas_affected = [['Central America', 'Mexico', 'Cuba', 'Florida', 'The Bahamas'
                   ['Central America', 'United States Gulf Coast (especially Florida Panhandle)']]
 
 # damages (USD($)) of hurricanes
-damages = ['Damages not recorded', '100M', 'Damages not recorded', '40M', '27.9M', '5M', 'Damages not recorded', '306M',
-           '2M', '65.8M', '326M', '60.3M', '208M', '1.42B', '25.4M', 'Damages not recorded', '1.54B', '1.24B', '7.1B',
-           '10B', '26.5B', '6.2B', '5.37B', '23.3B', '1.01B', '125B', '12B', '29.4B', '1.76B', '720M', '15.1B', '64.8B',
-           '91.6B', '25.1B']
+damages = [
+    'Damages not recorded', '100M', 'Damages not recorded', '40M', '27.9M', '5M', 'Damages not recorded', '306M',
+    '2M', '65.8M', '326M', '60.3M', '208M', '1.42B', '25.4M', 'Damages not recorded', '1.54B', '1.24B', '7.1B',
+    '10B', '26.5B', '6.2B', '5.37B', '23.3B', '1.01B', '125B', '12B', '29.4B', '1.76B', '720M', '15.1B', '64.8B',
+    '91.6B', '25.1B'
+]
 
 # deaths for each hurricane
-deaths = [90, 4000, 16, 3103, 179, 184, 408, 682, 5, 1023, 43, 319, 688, 259, 37, 11, 2068, 269, 318, 107, 65, 19325,
-          51, 124, 17, 1836, 125, 87, 45, 133, 603, 138, 3057, 74]
+deaths = [
+    90, 4000, 16, 3103, 179, 184, 408, 682, 5, 1023, 43, 319, 688, 259, 37, 11, 2068, 269, 318, 107, 65, 19325,
+    51, 124, 17, 1836, 125, 87, 45, 133, 603, 138, 3057, 74
+]
 
 # write your update damages function here:
 updated_damages = []
